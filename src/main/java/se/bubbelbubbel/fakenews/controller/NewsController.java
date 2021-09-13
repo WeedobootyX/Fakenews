6855ac3f37638e8ce657057ed8bce336a3ae32be
WeedobootyX <weedobooty@gmail.com>
2021-09-13 16:29:48 +0200

    La till cross origin

--- src/main/java/se/bubbelbubbel/fakenews/controller/NewsController.java
+++ src/main/java/se/bubbelbubbel/fakenews/controller/NewsController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import se.bubbelbubbel.fakenews.model.SnippetList;
 import se.bubbelbubbel.fakenews.model.star.PrinterPostRequest;
 import se.bubbelbubbel.fakenews.service.NewsService;
 
+@CrossOrigin
 @RestController
 public class NewsController {
 	Logger logger = LoggerFactory.getLogger(NewsController.class);
