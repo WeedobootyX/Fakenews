b4dbdd1368edfba4aecb252b1261cc48912927fb
WeedobootyX <weedobooty@gmail.com>
2021-10-06 15:04:34 +0200

    New exception

---
+++ src/main/java/se/bubbelbubbel/fakenews/exception/IllegalNewsflashException.java
@@ -0,0 +1,12 @@
+package se.bubbelbubbel.fakenews.exception;
+
+public class IllegalNewsflashException extends Exception {
+
+	public IllegalNewsflashException() {
+	}
+
+	public IllegalNewsflashException(String msg) {
+		super(msg);
+	}
+
+}
