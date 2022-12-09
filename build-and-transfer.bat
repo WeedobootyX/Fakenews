368fe5e457cb47ea78359e404a010c4410008c46
WeedobootyX <weedobooty@gmail.com>
2022-12-09 11:50:28 +0100

    Dockerfile and build script cleanup

--- build-and-transfer.bat
+++ build-and-transfer.bat
@@ -1,5 +1,5 @@
 ECHO STARTING BUILD
 call mvn clean install
-ECHO BUILD COMPLETED TRANSFERRING JAR FILE
-gcloud compute scp c:\git\fakenews\target\fakenews-0.1.jar wplab:/tmp
-PAUSE
+# ECHO BUILD COMPLETED TRANSFERRING JAR FILE
+# gcloud compute scp c:\git\fakenews\target\fakenews-0.1.jar wplab:/tmp
+# PAUSE
